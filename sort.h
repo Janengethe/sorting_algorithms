569f4a85f3be579fd1a96fb2e8c92f51ba46c0bd
Janengethe <janengethew@gmail.com>
2021-06-14 12:56:37 +0000

    initial commit

--- sort.h
+++ sort.h
@@ -1,6 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,6 +17,41 @@ const int n;
 struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
+
+typedef enum kind_e
+  {
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+  } kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+  const char *value;
+  const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+  const card_t *card;
+  struct deck_node_s *prev;
+  struct deck_node_s *next;
+} deck_node_t;
 
 /*print array and print list prototypes*/
 void print_array(const int *array, size_t size);

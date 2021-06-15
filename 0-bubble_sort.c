4e0515fc70347413d935bc8e6f95473470c977a0
Janengethe <janengethew@gmail.com>
2021-06-15 18:20:59 +0000

    correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, newsize = size;
-int tmp, swap;
+size_t i, j;
+int temp;
 
-if (array == NULL || size < 2)
-return;
-
-while (newsize)
+for (i = 0; i < size; i++)
 {
-swap = 0;
-for (i = 0; i < newsize - 1; i++)
+for (j = 0; j < size - 1; j++)
 {
-if (array[i] > array[i + 1])
+if (array[j] > array[j + 1])
 {
-tmp = array[i + 1];
-array[i + 1] = array[i];
-array[i] = tmp;
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
 print_array(array, size);
-swap = 1;
 }
 }
-newsize--;
-if (!swap)
-break;
 }
 }
